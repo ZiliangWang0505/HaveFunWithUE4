6e0c8a2e3e1be3937bd075634eb38542b7dec591
Wang Ziliang <wangziliang01@corp.netease.com>
2019-12-14 13:54:18 +0800

    自定义蓝图资源插件
    创建一个Editor插件，执行对蓝图资源的自定义操作。

---
+++ CustomBlueprintPlugin/CustomBPPlugin/Source/CustomBPPlugin/CustomBPPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomBPPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMBPPLUGIN_API ACustomBPPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
