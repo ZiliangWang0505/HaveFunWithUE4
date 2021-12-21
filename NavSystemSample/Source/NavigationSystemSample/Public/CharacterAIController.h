32abb3b8deb42d4f09b90ee0bec73ec192ad871d
AtticusWang <wangziliang05@gmail.com>
2021-12-21 15:02:57 +0800

    chore: generate vs project

---
+++ NavSystemSample/Source/NavigationSystemSample/Public/CharacterAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CharacterAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAVIGATIONSYSTEMSAMPLE_API ACharacterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
